c67fe667ece747ed5f352d13df452aa0750db466
venky916 <venkateshsmsv1999@gmail.com>
2024-09-03 14:43:02 +0530

    cors issue

--- index.js
+++ index.js
@@ -14,7 +14,15 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://trello-frontend-one.vercel.app'
+}));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://trello-frontend-one.vercel.app");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
 
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
